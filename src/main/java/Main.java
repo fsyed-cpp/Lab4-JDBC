4a54e95cda5097d44b5a3f4e8cc129aa6b4c76bd
fsyed-cpp <fsyed@cpp.edu>
2022-07-28 16:27:58 -0700

    Added query for problem 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,14 @@ public class Main {
     static final String username = "root";
 
     // ENTER YOUR PASSWORD HERE
-    static final String password = "Deys@1357";
+    static final String password = "";
 
     public static void main(String[] args) throws SQLException {
 
         //Scanner scan = new Scanner(System.in);
        // System.out.print("Please enter an Employee name: ");
        // String empName = scan.next();
-        displayTrips("Pomona", "Ontario", "7/16/2020");
+        Main.displayTrips("Pomona", "Ontario", "7/16/2020");
 
        // Integer empId = getEmployeeIdFromName(empName);
        // System.out.println(empId);
@@ -31,8 +31,8 @@ public class Main {
 
         // Get all the trips for a given start location, destination and date
         String query = "SELECT T1.StartLocationName, T1.DestinationName, T2.Date, T2.ScheduledStartTime, T2.ScheduledArrivalTime, T2.Drivername, T2.BusID\n" +
-                "FROM Trip T1, TripOffering T2\n" +
-                "WHERE T1.TripNumber = T2.TripNumber AND T1.StartLocationName =? AND T1.DestinationName = ? AND T2.Date = ?";
+                "FROM school.Trip T1, school.TripOffering T2\n" +
+                "WHERE T1.TripNumber = T2.TripNumber AND T1.StartLocationName = ? AND T1.DestinationName = ? AND T2.Date = ?";
 
         // Sample test data
         PreparedStatement stmt = con.prepareStatement(query);
@@ -43,14 +43,41 @@ public class Main {
         ResultSet resultSet = stmt.executeQuery();
 
         System.out.println("Displaying all trips that start at Pomona, end at Ontario, and started on 7/16/2020");
+        int tripNumber = 1;
         while (resultSet.next()) {
-            System.out.print("Trip number:");
-            System.out.println(resultSet.getInt("TripNumber"));
-            System.out.println("StartLocationName:");
-            System.out.println(resultSet.getInt("StartLocationName"));
-            System.out.println("DestinationName:");
-            System.out.println(resultSet.getInt("DestinationName"));
-            break;
+
+            System.out.println("Trip " + tripNumber);
+
+            System.out.print("StartLocation: ");
+            System.out.print(resultSet.getString("StartLocationName"));
+            System.out.print(" ");
+
+            System.out.print("DestinationName: ");
+            System.out.print(resultSet.getString("DestinationName"));
+            System.out.print(" ");
+
+            System.out.print("Date: ");
+            System.out.print(resultSet.getString("Date"));
+            System.out.print(" ");
+
+            System.out.print("ScheduledStartTime: ");
+            System.out.print(resultSet.getString("ScheduledStartTime"));
+            System.out.print(" ");
+
+            System.out.print("ScheduledArrivalTime: ");
+            System.out.print(resultSet.getString("ScheduledArrivalTime"));
+            System.out.print(" ");
+
+            System.out.print("Drivername: ");
+            System.out.print(resultSet.getString("Drivername"));
+            System.out.print(" ");
+
+            System.out.print("BusID:");
+            System.out.print(resultSet.getString("BusID"));
+            System.out.print(" ");
+
+            System.out.println();
+            tripNumber++;
         }
     }
 
